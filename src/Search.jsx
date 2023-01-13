2c19f02f5278ec427f81b853f3e55bb34424f111
Isaac Robert <isaac@sloovi.com>
2023-01-14 00:19:53 +0100

    Search feature

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,9 +1,54 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import search_icon from './assets/search-noir.svg';
+
+const Card = ({name, artists, link, cover_art}) => {
+
+    return (
+        <div className='music-card'>
+
+        </div>
+    )
+}
 
 const Search = () => {
+    const [results, setResults] = useState([]);
+    const [searchKeyword, setKeyword] = useState("");
+
+    const Search = async () => {
+        const {data} = axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Auth,orization: `Bearers ${token}`
+            },
+            params: {
+                type: "track",
+                q: searchKeyword
+            }
+        });
+
+        setResults(data.tracks.items)
+    }
     return (
         <div className='screen' id='search'>
-            Search
+            <div id={"search-bar"}>
+                <img src={search_icon} alt={"search"}/>
+                <input type={"text"} placeholder={"What do you want to listen to?"}/>
+            </div>
+            <div className='search-results'>
+                {
+                    results?.length > 0 ? (
+                        results.map(
+                            (res) => (
+                                <Card />
+                            )
+                        )
+                        
+                    ) : (
+                        <div>No matches found!</div>
+                    )
+                }
+            </div>
         </div>
     )
 }
